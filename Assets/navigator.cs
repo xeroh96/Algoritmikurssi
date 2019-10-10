3a9c49943ef7238a880e1d1679d088f8697735aa
xeroh96 <55236829+xeroh96@users.noreply.github.com>
2019-10-10 16:33:26 +0300

    Initial commit

---
+++ Assets/navigator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class navigator : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<NavMeshAgent>().destination = target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
