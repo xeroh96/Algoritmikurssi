b3de6876bf5c5dcd5ba4d7912eca50a972bd9de6
xeroh96 <55236829+xeroh96@users.noreply.github.com>
2019-10-24 15:28:33 +0300

    Päivitys
    kamera seuraa

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float lookSpeed = 10;
+    private Vector3 curLoc;
+    private Vector3 prevLoc;
+
+    void Update()
+    {
+        InputListen();
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position - prevLoc), Time.fixedDeltaTime * lookSpeed);
+    }
+
+    private void InputListen()
+    {
+        prevLoc = curLoc;
+        curLoc = transform.position;
+
+        if (Input.GetKey(KeyCode.A))
+            curLoc.x -= 1 * Time.fixedDeltaTime;
+        if (Input.GetKey(KeyCode.D))
+            curLoc.x += 1 * Time.fixedDeltaTime;
+        if (Input.GetKey(KeyCode.W))
+            curLoc.z += 1 * Time.fixedDeltaTime;
+        if (Input.GetKey(KeyCode.S))
+            curLoc.z -= 1 * Time.fixedDeltaTime;
+
+        transform.position = curLoc;
+
+    }
+}
